002be4d8e8cae24d510aee5b961c4bb5ddf4105b
Fyd <21723614@qq.com>
2025-08-22 16:42:04 +0800

    edit interface

--- program_parser/interface.go
+++ program_parser/interface.go
@@ -8,5 +8,24 @@ import (
 type Instruction interface {
 	TypeID() binary.TypeID
 	SetAccounts(accounts solana.PublicKeySlice) (err error)
-	Copy() Instruction
+	NewInstance() Instruction
+}
+
+type Parser interface {
+	ProgramID() solana.PublicKey
+	InstructionIdType() binary.TypeIDEncoding
+	Instructions() map[binary.TypeID]Instruction
+	Process(instruction any, tx Transaction) error
+}
+
+type Transaction interface {
+	Raw() bool
+	GetSlot() uint64
+	Signature() solana.Signature
+	FeePayer() solana.PublicKey
+	GetAccount(index int) (solana.PublicKey, error)
+	GetPublicKeys(indexes []byte) (solana.PublicKeySlice, error)
+	GetAccountMetas(indexes []byte) (solana.AccountMetaSlice, error)
+	GetFinalSOLBalanceAmount(account solana.PublicKey) uint64
+	GetFinalTokenBalanceAmount(account solana.PublicKey) uint64
 }
